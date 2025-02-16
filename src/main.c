6a56ec7ade6f6a0414e6bfde109f672860e7bae3
wifity <wifity@qq.com>
2025-02-16 16:29:59 +0800

    重新配置了c/c++ 支持 sdcc 中断语法，增加硬件文档和sdcc文档，写了一个基于时间中断的led闪烁程序

--- src/main.c
+++ src/main.c
@@ -1,16 +1,50 @@
-#include <mcs51/8051.h>
+#include <mcs51/8052.h>
 
-void delay_ms(unsigned int ms) {
-    unsigned int i, j;
-    for(i=0; i<ms; i++)
-        for(j=0; j<1275; j++);
+#define OSC_FREQ 12000000UL
+
+#define TIMER_MODE0 0x00
+#define TIMER_MODE1 0x01
+#define TIMER_MODE2 0x02
+#define TIMER_MODE3 0x03
+
+volatile unsigned int count = 0;
+
+void timer0_isr(void) __interrupt(1)
+{
+    count++;
+
+    TH0 = (65536 - 1000 * (OSC_FREQ / 12000000)) >> 8;
+    TL0 = (65536 - 1000 * (OSC_FREQ / 12000000)) & 0x00FF;
 }
 
-void main(void) {
-    while(1) {
-        P3_5 = 0;  // LED 亮
-        delay_ms(100);
-        P3_5 = 1;  // LED 灭
-        delay_ms(100);
+void timer0_init(void)
+{
+    TMOD &= 0xF0;
+    TMOD |= TIMER_MODE1; /* Timer 0 work on 16-bit timer mode */
+    TH0 = (65536 - 1000 * (OSC_FREQ / 12000000)) >> 8;
+    TL0 = (65536 - 1000 * (OSC_FREQ / 12000000)) & 0x00FF;
+    ET0 = 1; /* Enable timer 0 interrupt */
+    EA = 1;
+    TR0 = 1; /* Turn on timer 0 */
+}
+#define led1 P3_5
+
+void delayms(unsigned int n)
+{
+    while (count <= n)
+        ;
+
+    count = 0;
+}
+void main(void)
+{
+    timer0_init();
+
+    while (1)
+    {
+        led1 = 0;
+        delayms(1000);
+        led1 = 1;
+        delayms(1000);
     }
 }
